7c276800819437d3846b0aa5b8171c0544570924
JARED ARKIN <ARKINJB@GMAIL.COM>
2015-08-31 19:51:20 -0400

    added basic structure-controllers,views,routes,seeds

---
+++ app/controllers/spreads_controller.rb
@@ -0,0 +1,21 @@
+class SpreadsController < ApplicationController
+
+  #index
+  def index
+  end
+
+  #new
+  def new
+  end
+  
+  #create
+
+  #show
+
+  #edit
+
+  #update
+
+  #destroy
+
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,38 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+Spread.destroy_all
+Record.destroy_all
+WeeklyPick.destroy_all
+
+Spread.create([
+  {game_id: 2015091000, team: "NE", spread: 3},
+  {game_id: 2015091000, team: "PIT", spread: -3},
+  {game_id: 2015091300, team: "CHI", spread: 6},
+  {game_id: 2015091300, team: "GB", spread:-6},
+  {game_id: 2015091301, team: "STL", spread:10},
+  {game_id: 2015091301, team: "SEA", spread:-10},
+  {game_id: 2015091302, team: "JAC", spread: 7.5},
+  {game_id: 2015091302, team: "CAR", spread:-7.5},
+  {game_id: 2015091303, team: "WAS", spread:5},
+  {game_id: 2015091303, team: "MIA", spread:-5},
+  {game_id: 2015091304, team: "BUF", spread:6.5},
+  {game_id: 2015091304, team: "IND", spread:-6.5},
+  {game_id: 2015091305, team: "NYJ", spread:0},
+  {game_id: 2015091305, team: "CLE", spread:0},
+  {game_id: 2015091306, team: "HOU", spread:2},
+  {game_id: 2015091306, team: "KC", spread:-2},
+  {game_id: 2015091307, team: "ARI", spread:3},
+  {game_id: 2015091307, team: "NO", spread:-3},
+  {game_id: 2015091308, team: "SD", spread:-2},
+  {game_id: 2015091308, team: "DET", spread:2},
+  {game_id: 2015091309, team: "DEN", spread:-3},
+  {game_id: 2015091309, team: "BAL", spread:3},
+  {game_id: 2015091310, team: "OAK", spread:8},
+  {game_id: 2015091310, team: "CIN", spread:-8},
+  {game_id: 2015091311, team: "TB", spread:2.5},
+  {game_id: 2015091311, team: "TEN", spread:-2.5},
+  {game_id: 2015091312, team: "DAL", spread:1},
+  {game_id: 2015091312, team: "NYG", spread:-1},
+  {game_id: 2015091400, team: "ATL", spread:4.5},
+  {game_id: 2015091400, team: "PHI", spread:-4.5},
+  {game_id: 2015091401, team: "SF", spread:3},
+  {game_id: 2015091401, team: "MIN", spread:-3}
+  ])
